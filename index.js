524c12d97c295333ea344365f44087979a8abd01
dthapa770 <dthapa@mail.sfsu.edu>
2022-03-28 17:14:21 -0700

    beautified page using flexbox

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const dom_selectors={
     album_container: document.querySelector('.album_container'),
     search_text:document.querySelector('.search-bar__input').value,
     search_form: document.querySelector(".search-form"),
-    message_text: document.getElementById('message-text')
-   
+    message_text: document.getElementById('message-text')   
 }
 dom_selectors.search_form.addEventListener("submit", (e) =>{
     e.preventDefault()
@@ -15,13 +14,12 @@ dom_selectors.search_form.addEventListener("submit", (e) =>{
 })
   
 async function albums(artist){
-
     const response = await fetchJsonp(`https://itunes.apple.com/search?term=${artist}&media=music&entity=album&attribute=artistTerm&limit=500`);
     const result =  await response.json()
     albums = await result.results
+    length = albums.length;
+    dom_selectors.message_text.innerHTML =` ${length} results found...`
     display_albums(albums)
-
-
 };
 
 const display_albums = (albums) =>{
